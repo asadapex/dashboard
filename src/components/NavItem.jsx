785c0d2ff0902cc140d0e0de9d960e63e219d56c
Asadbek <admin@MacBook-Pro--Asadbek.local>
2025-06-19 15:47:37 +0500

    header done

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-const NavItem = ({item}) => {
-    return (
-                    <NavLink className={`flex items-center gap-[24px] relative text-[16px] font-normal text-[#A4A6B3] py-[20px] pl-[32px]`} to={item.path}>
-                        {item.icon}
-                        <span>{item.title}</span>
-                    </NavLink>
-    )
-}
+const NavItem = ({ item }) => {
+  console.log(item);
+  return (
+    <NavLink
+      className={`flex items-center gap-[24px] relative text-[16px] font-normal text-[#A4A6B3] py-[20px] pl-[32px]`}
+      to={item.path}
+    >
+      {item.icon}
+      <span>{item.title}</span>
+    </NavLink>
+  );
+};
 
-export default NavItem
+export default NavItem;
--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { navList } from "../hooks/paths";
+import { NotificationIcon, SearchIcon, StickIcon } from "../assets/icons";
+import AvatarIMG from "../assets/images/avatar.png";
 
 const Header = () => {
+  const { pathname } = useLocation();
 
-    const {pathname} = useLocation()
+  return (
+    <div className="flex items-center justify-between mb-[58px]">
+      <strong className="font-bold text-[24px] text-[#252733]">
+        {navList.find((item) => item.path == pathname).title}
+      </strong>
+      <div className="flex items-center gap-[24px]">
+        <SearchIcon />
+        <NotificationIcon />
+        <StickIcon />
+        <strong>Jones Ferdinand</strong>
+        <img src={AvatarIMG} alt="" />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="flex items-center justify-between mb-[58px]">
-            <strong className="font-bold text-[24px] text-[#252733]">{navList.find(item => item.path == pathname).title}</strong>
-            <div></div>
-        </div>
-    )
-}
-
-export default Header
+export default Header;
